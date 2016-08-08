4e715653a17b169613198eedb21543afb6709a8f
Kyle Altendorf <sda@fstab.net>
2016-08-08 05:06:53 -0700

    Use just `python35.dll` instead of full path
    This change is in the instructions to build the PyQt5
    Designer plugin so no apparent changes in this code.

--- buildinfo.py
+++ buildinfo.py
@@ -62,7 +62,7 @@ version_str = (
     '.'.join([str(v) for v in version_numbers])
     + status.prerelease_suffix
     # TODO: this is totally cheating, revisit the revision scheme
-    + '2'# prerelease_str
+    + '3'# prerelease_str
 )
 
 directories = [
