1163f50baadca848f3f8059aeda3bb7ae3ad86f0
Kyle Altendorf <sda@fstab.net>
2016-08-14 11:19:33 -0700

    Bump rev to v5.7.dev9

--- buildinfo.py
+++ buildinfo.py
@@ -62,7 +62,7 @@ version_str = (
     '.'.join([str(v) for v in version_numbers])
     + status.prerelease_suffix
     # TODO: this is totally cheating, revisit the revision scheme
-    + '8'# prerelease_str
+    + '9'# prerelease_str
 )
 
 directories = [
