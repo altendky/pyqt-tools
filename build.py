3980a1d087a77ce9c920b0c0de60fcf779eb0c98
Kyle Altendorf <kyle.altendorf@epcpower.com>
2017-07-18 08:58:52 -0700

    Try extracting the Python module code (fix)

--- build.py
+++ build.py
@@ -364,6 +364,7 @@ plat-name = {plat_name}'''.format(**locals()))
         report_and_check_call(
             command=[
                 'patch',
+                os.path.join(pyqt5, 'designer', 'pluginloader.cpp'),
                 'pluginloader.patch',
             ],
         )
