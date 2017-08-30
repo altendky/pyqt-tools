907b2028b5feeb64c1d48a83daa99e067ba1865f
Kyle Altendorf <sda@fstab.net>
2017-08-30 11:12:15 -0400

    Bare start at Travis/Linux build

---
+++ buildlinux.py
@@ -0,0 +1,41 @@
+import os
+import shutil
+import subprocess
+import sys
+
+
+def report_and_check_call(command, *args, **kwargs):
+    print('\nCalling: {}'.format(command))
+    sys.stdout.flush()
+    subprocess.check_call(command, *args, **kwargs)
+
+
+def main():
+    qt_bin_path = os.path.join('Qt', '5.9.1', 'gcc_64', 'bin')
+
+    build = os.environ['TRAVIS_BUILD_DIR']
+    destination = os.path.join(build, 'pyqt5-tools')
+    os.makedirs(destination, exist_ok=True)
+
+    deployqt_path = 'linuxdeployqt-continuous-x86_64.AppImage'
+
+    for application in os.listdir(qt_bin_path):
+        application_path = os.path.join(qt_bin_path, application)
+
+        shutil.copy(application_path, destination)
+
+        report_and_check_call(
+            command=[
+                deployqt_path,
+                application,
+                '-qmake={}'.format(os.path.join(qt_bin_path, 'qmake')),
+            ],
+            cwd=destination,
+        )
+
+    report_and_check_call(
+        command=[
+            'tree'
+        ],
+        cwd=destination,
+    )
