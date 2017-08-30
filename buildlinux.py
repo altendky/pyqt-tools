9b6251c6d2cde155e360f276a2cf1f62c49fd8e6
Kyle Altendorf <sda@fstab.net>
2017-08-30 14:08:48 -0400

    Actually execute something in buildlinux.py

--- buildlinux.py
+++ buildlinux.py
@@ -6,11 +6,13 @@ import sys
 
 def report_and_check_call(command, *args, **kwargs):
     print('\nCalling: {}'.format(command))
+    # may only be required on AppVeyor
     sys.stdout.flush()
     subprocess.check_call(command, *args, **kwargs)
 
 
 def main():
+    print('in {}'.format(__file__))
     qt_bin_path = os.path.join('Qt', '5.9.1', 'gcc_64', 'bin')
 
     build = os.environ['TRAVIS_BUILD_DIR']
@@ -39,3 +41,7 @@ def main():
         ],
         cwd=destination,
     )
+
+
+if __name__ == '__main__':
+    sys.exit(main())
