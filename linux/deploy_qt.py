f20c3187bca49eba9bfc08f06b7f22c688ba6760
Kyle Altendorf <sda@fstab.net>
2017-08-31 14:53:01 -0400

    ...

--- linux/deploy_qt.py
+++ linux/deploy_qt.py
@@ -4,19 +4,23 @@ import subprocess
 import sys
 
 
-def report_and_check_call(command, *args, **kwargs):
-    print('\nCalling: {}'.format(command))
+def report_and_check_call(**kwargs):
+    print('\nCalling: ')
+
+    for kwarg in ('command', 'cwd'):
+        if kwarg in kwargs:
+            print('    {}: {}'.format(kwarg, kwargs[kwargs]))
+
     # may only be required on AppVeyor
     sys.stdout.flush()
-    subprocess.check_call(command, *args, **kwargs)
+    subprocess.check_call(**kwargs)
 
 
 def main():
     build = os.environ['TRAVIS_BUILD_DIR']
     qt_bin_path = os.path.join(build, 'Qt', '5.9.1', 'gcc_64', 'bin')
     deployed_qt = os.path.join(build, 'deployed_qt')
-    destination = os.path.join(build, 'pyqt5-tools')
-    os.makedirs(destination, exist_ok=True)
+    os.makedirs(deployed_qt, exist_ok=True)
 
     deployqt_path = os.path.join(
         build,
@@ -46,6 +50,8 @@ def main():
             print('FAILED SO SKIPPING: {}'.format(application))
             os.remove(os.path.join(deployed_qt, application))
             skipped.append(application)
+
+    os.remove(os.path.join(deployed_qt, 'AppRun'))
 
     print('\nSkipped: ')
     print('\n'.join('    {}'.format(a for a in sorted(skipped))))
