7fc58754a10c9114f9db4f962207a79b8f01050f
Kyle Altendorf <sda@fstab.net>
2017-09-01 11:45:10 -0400

    print what copying to where

--- buildlinux.py
+++ buildlinux.py
@@ -12,6 +12,8 @@ if __name__ == '__main__':
     deployed_qt = os.path.join(build, 'deployed_qt')
     package = os.path.join(build, 'pyqt5-tools')
 
+    print('copying {}'.format(deployed_qt))
+    print('     to {}'.format(package))
     shutil.copytree(deployed_qt, package)
 
     sys.exit(main())
