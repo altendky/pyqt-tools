40e7d8450f11434f0fe703dbce5e9c17925a65bf
Kyle Altendorf <kyle.altendorf@epcpower.com>
2018-09-05 08:34:43 -0700

    add bad plugin for testing the exception dialog

--- src/pyqt5_tools/entrypoints.py
+++ src/pyqt5_tools/entrypoints.py
@@ -14,7 +14,9 @@ here = pathlib.Path(__file__).parent
 example_path = str(
     pathlib.Path(pyqt5_tools.examplebuttonplugin.__file__).parent,
 )
-
+bad_path = str(
+    pathlib.Path(pyqt5_tools.badplugin.__file__).parent,
+)
 
 def pyqt5toolsinstalluic():
     destination = here/'bin'
@@ -51,16 +53,30 @@ def pyqt5toolsinstalluic():
     help='Pass through to get Designer\'s --help',
     is_flag=True,
 )
-def pyqt5designer(ctx, widget_paths, designer_help, example_widget_path):
+@click.option(
+    '--test-exception-dialog',
+    help='Raise an exception to check the exception dialog functionality.',
+    is_flag=True,
+)
+def pyqt5designer(
+        ctx,
+        widget_paths,
+        designer_help,
+        example_widget_path,
+        test_exception_dialog,
+):
     dotenv.load_dotenv()
-
     extras = []
+    widget_paths = list(widget_paths)
 
     if designer_help:
         extras.append('--help')
 
     if example_widget_path:
-        widget_paths = [*widget_paths, example_path]
+        widget_paths.append(example_path)
+
+    if test_exception_dialog:
+        widget_paths.append(bad_path)
 
     env = dict(os.environ)
     env['PYQTDESIGNERPATH'] = (
