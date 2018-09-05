75d83afdb89174a023695300d1130d840da58eb2
Kyle Altendorf <kyle.altendorf@epcpower.com>
2018-09-05 15:01:14 -0400

    upload to pypi on tags

---
+++ twineontag.py
@@ -0,0 +1,35 @@
+from __future__ import print_function
+
+import subprocess
+import sys
+
+
+def main():
+    no_tag = subprocess.call(
+        [
+            'git',
+            'describe',
+            '--tags',
+            '--candidates',
+            '0',
+        ],
+    )
+
+    if no_tag:
+        print('No tag found, doing nothing.')
+        return
+
+    print('Tag found, uploading to PyPI.')
+
+    subprocess.call(
+        [
+            'twine',
+            'upload',
+            '*.whl',
+        ],
+        check=True,
+    )
+
+
+if __name__ == '__main__':
+    sys.exit(main())
