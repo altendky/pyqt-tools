47436981f14fbc2149d8db2cf81bdd61f008c2a4
Kyle Altendorf <kyle.altendorf@epcpower.com>
2018-10-20 19:53:11 -0400

    debug and ...

--- src/pyqt5_tools/tests/test_designer.py
+++ src/pyqt5_tools/tests/test_designer.py
@@ -11,19 +11,25 @@ import pyqt5_tools.tests.testbuttonplugin
 
 def test_creates_test_widget(tempdir):
     env = dict(os.environ)
-    path = pathlib.Path(tempdir)/'tigger'
-    env[pyqt5_tools.tests.testbutton.test_path_env_var] = path
+    file_path = pathlib.Path(tempdir)/'tigger'
+    env[pyqt5_tools.tests.testbutton.test_path_env_var] = file_path
+
+    widget_plugin_path = pathlib.Path(
+        pyqt5_tools.tests.testbuttonplugin.__file__,
+    ).parent
+
+    print('env:', env)
 
     with pytest.raises(subprocess.TimeoutExpired):
         subprocess.run(
             [
                 str(pathlib.Path(sys.executable).with_name('pyqt5designer')),
-                '--widget-path', str(pathlib.Path(pyqt5_tools.tests.testbuttonplugin.__file__).parent),
+                '--widget-path', str(widget_plugin_path),
             ],
             check=True,
             env=env,
             timeout=30,
         )
 
-    with open(path, 'rb') as f:
+    with open(file_path, 'rb') as f:
         assert f.read() == pyqt5_tools.tests.testbutton.test_file_contents
