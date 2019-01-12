8e7d85bbf7808c3d2084616c93c9ab9c2635824b
Kyle Altendorf <kyle.altendorf@epcpower.com>
2019-01-12 13:58:45 -0500

    add test for qml plugin

--- src/pyqt5_tools/examples/qmltext.py
+++ src/pyqt5_tools/examples/qmltext.py
@@ -1,5 +1,12 @@
-from PyQt5 import QtGui
+import os
+import pathlib
+
 from PyQt5 import QtQuick
+
+
+test_path_env_var = 'PYQT5TOOLS_TEST_PATH'
+test_file_contents = b'jagular'
+write_for_test = test_path_env_var in os.path
 
 
 class Text(QtQuick.QQuickPaintedItem):
@@ -7,6 +14,15 @@ class Text(QtQuick.QQuickPaintedItem):
         super().__init__(*args, **kwargs)
 
     def paint(self, painter):
+        global write_for_test
+
+        if write_for_test:
+            write_for_test = False
+
+            path = pathlib.Path(os.environ[test_path_env_var])
+            with path.open('xb') as f:
+                f.write(test_file_contents)
+
         painter.drawText(
             self.width() / 2, 
             self.height() / 2, 
--- src/pyqt5_tools/tests/testbutton.py
+++ src/pyqt5_tools/tests/testbutton.py
@@ -1,17 +1,25 @@
 import os
+import pathlib
 
 from PyQt5 import QtWidgets
 
 
 test_path_env_var = 'PYQT5TOOLS_TEST_PATH'
 test_file_contents = b'heffalump'
+write_for_test = test_path_env_var in os.path
 
 
 class TestButton(QtWidgets.QPushButton):
     def __init__(self, parent):
+        global write_for_test
+
         super().__init__(parent)
 
         self.setText('pyqt5-tools Test Button')
 
-        with open(os.environ[test_path_env_var], 'xb') as f:
-            f.write(test_file_contents)
+        if write_for_test:
+            write_for_test = False
+
+            path = pathlib.Path(os.environ[test_path_env_var])
+            with path.open('xb') as f:
+                f.write(test_file_contents)
