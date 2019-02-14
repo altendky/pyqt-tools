1a1b66b1453d29a9a9e470baba941eaeb2822dbf
Kyle Altendorf <kyle.altendorf@epcpower.com>
2019-02-13 22:23:22 -0500

    more

--- build.py
+++ build.py
@@ -365,6 +365,7 @@ plat-name = {plat_name}'''.format(**locals()))
         '5.10.1': '4.19.8',
         '5.11.2': '4.19.13',
         '5.11.3': '4.19.13',
+        '5.12': , '4.19.14',
     }[pyqt5_version]
 
     sip_name = 'sip-{}'.format(sip_version)
