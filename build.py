2e0ac9e1ab48e161f15c7d5597fe8d0bef9b12e8
Kyle Altendorf <sda@fstab.net>
2019-04-01 13:44:53 -0400

    try again for 5.12 (.1 this time)

--- build.py
+++ build.py
@@ -366,6 +366,7 @@ plat-name = {plat_name}'''.format(**locals()))
         '5.11.2': '4.19.13',
         '5.11.3': '4.19.13',
         '5.12': '4.19.14',
+        '5.12.1': '4.19.15',
     }[pyqt5_version]
 
     sip_name = 'sip-{}'.format(sip_version)
