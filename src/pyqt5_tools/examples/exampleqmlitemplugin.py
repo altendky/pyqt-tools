54f0e49437533fd8dcb284d3e6e0f5339a1f414d
Kyle Altendorf <sda@fstab.net>
2020-04-03 22:37:18 -0400

    flush it

--- src/pyqt5_tools/examples/exampleqmlitemplugin.py
+++ src/pyqt5_tools/examples/exampleqmlitemplugin.py
@@ -1,17 +1,22 @@
 import sys
 sys.stderr.write('exampleqmlitemplugin.py debug: : just imported sys')
+sys.stderr.flush()
 import traceback
 sys.stderr.write('exampleqmlitemplugin.py debug: : just imported traceback')
+sys.stderr.flush()
 from PyQt5 import QtQml
 sys.stderr.write('exampleqmlitemplugin.py debug: : just imported QtQml')
+sys.stderr.flush()
 
 import pyqt5_tools.examples.exampleqmlitem
 sys.stderr.write('exampleqmlitemplugin.py debug: : just imported pyqt5_tools.examples.exampleqmlitem')
+sys.stderr.flush()
 
 
 class ExampleQmlItemPlugin(QtQml.QQmlExtensionPlugin):
     def registerTypes(self, uri):
         sys.stderr.write('exampleqmlitemplugin.py debug: ExampleQmlItemPlugin.registerTypes(): uri - {!r}'.format(uri))
+        sys.stderr.flush()
         try:
             QtQml.qmlRegisterType(
                 pyqt5_tools.examples.exampleqmlitem.ExampleQmlItem,
@@ -22,8 +27,11 @@ class ExampleQmlItemPlugin(QtQml.QQmlExtensionPlugin):
             )
         except Exception as e:
             sys.stderr.write('exampleqmlitemplugin.py debug: ExampleQmlItemPlugin.registerTypes(): exception - {!r}'.format(e))
+            sys.stderr.flush()
             traceback.print_exc(file=sys.stderr)
+            sys.stderr.flush()
             raise
 
 
 sys.stderr.write('exampleqmlitemplugin.py debug: : import complete')
+sys.stderr.flush()
