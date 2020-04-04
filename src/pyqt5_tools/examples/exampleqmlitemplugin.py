e39ea254d48c6b86584fe2a270838094af3804a5
Kyle Altendorf <sda@fstab.net>
2020-04-03 22:56:55 -0400

    \n

--- src/pyqt5_tools/examples/exampleqmlitemplugin.py
+++ src/pyqt5_tools/examples/exampleqmlitemplugin.py
@@ -1,21 +1,21 @@
 import sys
-sys.stderr.write('exampleqmlitemplugin.py debug: : just imported sys')
+sys.stderr.write('exampleqmlitemplugin.py debug: : just imported sys\n')
 sys.stderr.flush()
 import traceback
-sys.stderr.write('exampleqmlitemplugin.py debug: : just imported traceback')
+sys.stderr.write('exampleqmlitemplugin.py debug: : just imported traceback\n')
 sys.stderr.flush()
 from PyQt5 import QtQml
-sys.stderr.write('exampleqmlitemplugin.py debug: : just imported QtQml')
+sys.stderr.write('exampleqmlitemplugin.py debug: : just imported QtQml\n')
 sys.stderr.flush()
 
 import pyqt5_tools.examples.exampleqmlitem
-sys.stderr.write('exampleqmlitemplugin.py debug: : just imported pyqt5_tools.examples.exampleqmlitem')
+sys.stderr.write('exampleqmlitemplugin.py debug: : just imported pyqt5_tools.examples.exampleqmlitem\n')
 sys.stderr.flush()
 
 
 class ExampleQmlItemPlugin(QtQml.QQmlExtensionPlugin):
     def registerTypes(self, uri):
-        sys.stderr.write('exampleqmlitemplugin.py debug: ExampleQmlItemPlugin.registerTypes(): uri - {!r}'.format(uri))
+        sys.stderr.write('exampleqmlitemplugin.py debug: ExampleQmlItemPlugin.registerTypes(): uri - {!r}\n'.format(uri))
         sys.stderr.flush()
         try:
             QtQml.qmlRegisterType(
@@ -26,12 +26,12 @@ class ExampleQmlItemPlugin(QtQml.QQmlExtensionPlugin):
                 'ExampleQmlItem',
             )
         except Exception as e:
-            sys.stderr.write('exampleqmlitemplugin.py debug: ExampleQmlItemPlugin.registerTypes(): exception - {!r}'.format(e))
+            sys.stderr.write('exampleqmlitemplugin.py debug: ExampleQmlItemPlugin.registerTypes(): exception - {!r}\n'.format(e))
             sys.stderr.flush()
             traceback.print_exc(file=sys.stderr)
             sys.stderr.flush()
             raise
 
 
-sys.stderr.write('exampleqmlitemplugin.py debug: : import complete')
+sys.stderr.write('exampleqmlitemplugin.py debug: : import complete\n')
 sys.stderr.flush()
