bce8663dfb0c767d338f0276568f0835a9a805fb
Kyle Altendorf <sda@fstab.net>
2020-10-18 21:32:23 -0400

    add test for translations being present

--- src/qt_tools/tests/test_entrypoints.py
+++ src/qt_tools/tests/test_entrypoints.py
@@ -35,3 +35,17 @@ def test_qmlscene():
             env={**os.environ, 'QT_DEBUG_PLUGINS': '1'},
             timeout=10,
         )
+
+
+def test_language():
+    completed_process = subprocess.run(
+        [
+            fspath(pathlib.Path(sys.executable).with_name('qtdiag')),
+        ],
+        check=True,
+        env={**os.environ, 'LANGUAGE': 'de_DE'},
+        stdout=subprocess.PIPE,
+        timeout=30,
+    )
+
+    assert b'de_DE' in completed_process.stdout
