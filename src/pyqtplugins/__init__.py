b8ffd7a812bcfc7561a944bb7e1ea4039d340743
Kyle Altendorf <kyle.altendorf@epcpower.com>
2020-10-25 19:18:22 -0700

    workaround qml wanting paths all lower case and .resolve() respecting actual case on windows

--- src/pyqtplugins/__init__.py
+++ src/pyqtplugins/__init__.py
@@ -1,9 +1,13 @@
+import os
 import pathlib
 
 import PyQt5
 
 
 root = pathlib.Path(__file__).resolve().parent
+# TODO: so apparently qml wants it all lower case...
+if sys.platform == 'win32':
+    root = pathlib.Path(os.fspath(root).lower())
 plugins = root.joinpath('Qt', 'plugins')
 
 pyqt5_root = pathlib.Path(PyQt5.__file__).resolve().parent
