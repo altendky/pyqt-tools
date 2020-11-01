8d64a97381592083b29fdbf95f5ea9a17b278a3b
Kyle Altendorf <sda@fstab.net>
2020-11-01 11:54:12 -0500

    work on versions and dependencies

--- setup.py
+++ setup.py
@@ -4,26 +4,37 @@ import setuptools
 import versioneer
 
 
+class InvalidVersionError(Exception):
+    pass
+
+
+def pad_version(v, segment_count=3):
+    split = v.split('.')
+    if len(split) > segment_count:
+        raise InvalidVersionError('{} has more than three segments'.format(v))
+
+    return '.'.join(split + ['0'] * (segment_count - len(split)))
+
+
 # TODO: really doesn't seem quite proper here and probably should come
 #       in some other way?
-os.environ.setdefault('PYQT_VERSION', '5.15.1')
-os.environ.setdefault('QT_VERSION', '5.15.1')
+pyqt_version = pad_version(os.environ.setdefault('PYQT_VERSION', '5.15.1'))
+qt_version = pad_version(os.environ.setdefault('QT_VERSION', '5.15.1'))
 
 
-def pad_version(v):
-    split = v.split('.')
-    return '.'.join(split + ['0'] * (3 - len(split)))
+pyqt5_tools_wrapper_version = pad_version(versioneer.get_versions()['version'])
+pyqt5_tools_version = '{}.{}'.format(pyqt_version, pyqt5_tools_wrapper_version)
 
 
-def calculate_version():
-    version = versioneer.get_versions()['version']
-
-    version = '.'.join((
-        pad_version(os.environ['PYQT_VERSION']),
-        version,
-    ))
-
-    return version
+# When using ~=, don't pad because that affects allowed versions
+pyqt_plugins_wrapper_version = '0.1.0'
+pyqt_plugins_version_specifier = '~={}.{}'.format(
+    pyqt_version,
+    pyqt_plugins_wrapper_version,
+)
+# such as:  @ git+https://github.com/altendky/pyqt5-tools@just_plugins
+# or empty when using a regular index
+pyqt_plugins_url = ' @ git+https://github.com/altendky/pyqt5-tools@just_plugins'
 
 
 with open('README.rst') as f:
@@ -59,14 +70,16 @@ setuptools.setup(
     ],
     packages=setuptools.find_packages('src'),
     package_dir={'': 'src'},
-    version=calculate_version(),
+    version=pyqt5_tools_version,
     include_package_data=True,
     python_requires=">=3.5",
     install_requires=[
         'click',
-        'pyqt5=={}'.format(os.environ['PYQT_VERSION']),
-        'pyqtplugins @ git+https://github.com/altendky/pyqt5-tools@just_plugins',
-        'qttools @ git+https://github.com/altendky/pyqt5-tools@just_applications',
+        'pyqt5=={}'.format(pyqt_version),
+        'pyqtplugins{}{}'.format(
+            pyqt_plugins_version_specifier,
+            pyqt_plugins_url,
+        ),
     ],
     entry_points={
         'console_scripts': [
